38c9052f33bd9dcbe323656d685b552e6598deb4
BooScript <tomw08@gmail.com>
2015-09-07 16:55:09 +0100

    Initial Set up

---
+++ src/CasperLinkScrape.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Tom on 04/09/2015.
+ */
+var links = [];
+var casper = require('casper').create();
+var fs = require('fs');
+
+
+
+function getProfileLinks() {
+    var links = document.querySelectorAll('a');
+    return Array.prototype.map.call(links, function(e) {
+        return e.getAttribute('href');
+    });
+}
+
+casper.start('https://www.lendwithcare.org/search/funded/completed/', function() {
+
+});
+
+casper.then(function(){
+    //store css selector for  button that loads more content
+    var buttonCss = '#card_container > div.paginate-more > button';
+
+    for(var i=0; i<4; i++) {
+
+        // when button appears echo
+        casper.waitForSelector(buttonCss,
+
+            function() {
+                this.echo('the button has appeared [waitforselector]');
+                this.click('#card_container > div.paginate-more > button');
+            },
+
+            function onTimeout(){this.echo('timed out before button element loaded in DOM')}, 15000
+        );
+    }
+});
+
+casper.then(function() {
+
+    // aggregate links from page
+    links = this.evaluate(getProfileLinks);
+
+    // write all links to JSON file
+    fs.write('linksData.json', JSON.stringify(links), 'w');
+
+});
+
+casper.then(function() {
+
+    //this.capture('google.png');
+    links = links.concat(this.evaluate(getProfileLinks));
+});
+
+casper.run(function() {
+    // echo results in some pretty fashion
+    this.echo(links.length + ' links found:');
+    this.echo(' - ' + links.join('\n - ')).exit();
+
+});
---
+++ src/Main.js
@@ -0,0 +1,40 @@
+// import modules.
+
+var fs = require('fs');
+
+require('./ProjectClass.js');
+var scrape = require('./profileScrape.js');
+
+
+// import function to scrape profile
+var GetProfileLinks = require('./GetProfileLinks_FromDump.js');
+
+// all profile urls consist of this url
+var baseURL = 'https://www.lendwithcare.org/';
+
+//test projattr class is exposed
+console.log( projectAttr);
+
+// get array of funded project profile links
+var linksArr = GetProfileLinks.getProfileLinks;
+
+//array to hold project data
+var profileDataRaw = [];
+
+
+//scrape profiles
+for (var i = 0; i < 20; i++ ) {
+    //concatenate base url with profile link url
+    var urlCur = baseURL + linksArr[i];
+
+    profileDataRaw.push(scrape.profileScrape(urlCur));
+}
+
+
+// array to hold individual profile data
+//var profilesArr = [];
+
+//loop through
+
+// write key project attributes to JSON file for completed projects
+//fs.write('CompletedProjects.json', JSON.stringify(), 'w');
