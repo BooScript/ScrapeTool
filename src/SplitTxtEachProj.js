6b48ab1a35874ec8a31bfa33a6da6ba2b45084f0
TomChambrier <tomw08@gmail.com>
2015-09-08 10:53:33 +0100

    reads raw dump and converts to json file

---
+++ src/SplitTxtEachProj.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Tom on 08/09/2015.
+ * Reads and splits rawProfileData.txt into array where elements holds raw project data
+ *
+ **/
+//import module
+var fs = require('fs');
+
+var fileContent;
+function readfile(callback){
+    fs.readFile('rawProfileContent.txt','utf-8', function(err, data){
+        if(err){
+            throw err;
+        }
+        fileContent = data;
+        // will call function to parse and write file when file has been read
+        callback()
+    });
+}
+
+//splits dump of data into array of individual projects
+function splitRawtoJSON() {
+    //split text by project
+    projectRawArr = fileContent.split(/<!DOCTYPE html>/g);
+    console.log(projectRawArr);
+    //write to json file
+    fs.writeFile('individualProjectsRaw.json', JSON.stringify(projectRawArr,'', '/n splitH'), function (err) {
+        if (err) throw err;
+    });
+}
+//read file then pass in parsing function to callback after file is read
+// then write array to JSON
+readfile(splitRawtoJSON);
