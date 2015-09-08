2b62997336b9704052f60c9a5693f091407ef596
TomChambrier <tomw08@gmail.com>
2015-09-08 10:55:03 +0100

    added location prop

--- src/ProjectClass.js
+++ src/ProjectClass.js
@@ -1,49 +1,50 @@
 /**
  * Created by Tom on 07/09/2015.
  */
-// class to stor individual project attributes
-projectAttr = {
-    name: null,
-    dateLoanAdded: null,
-    value: null,
-    repaymentTerm: null,
-    activityType: null,
-    mfiPartner: null,
-    description: null,
-    expectedPayments: {
-        month1: {
-            date: null,
-            value: null
+// class to store individual project attributes
+ projectAttr = {
+        name: null
+        location: null,
+        dateLoanAdded: null,
+        value: null,
+        repaymentTerm: null,
+        activityType: null,
+        mfiPartner: null,
+        description: null,
+        expectedPayments: {
+            month1: {
+                date: null,
+                value: null
+            },
+            month2: {
+                date: null,
+                value: null
+            },
+            month3: {
+                date: null,
+                value: null
+            },
+            month4: {
+                date: null,
+                value: null
+            }
         },
-        month2: {
-            date: null,
-            value: null
-        },
-        month3: {
-            date: null,
-            value: null
-        },
-        month4: {
-            date: null,
-            value: null
+        actualRepayments: {
+            month1: {
+                date: null,
+                value: null
+            },
+            month2: {
+                date: null,
+                value: null
+            },
+            month3: {
+                date: null,
+                value: null
+            },
+            month4: {
+                date: null,
+                value: null
+            }
         }
-    },
-    actualRepayments: {
-        month1: {
-            date: null,
-            value: null
-        },
-        month2: {
-            date: null,
-            value: null
-        },
-        month3: {
-            date: null,
-            value: null
-        },
-        month4: {
-            date: null,
-            value: null
-        }
-    }
-};
+    };
