c1c7cd3b787fa2bd4f69c5a72494e4ca2828ef68
TomChambrier <tomw08@gmail.com>
2015-09-08 10:06:33 +0100

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,8 @@
 // import modules.
+//var Q = require('q');
 
 var fs = require('fs');
-
+var Promise = require("bluebird");
 require('./ProjectClass.js');
 var scrape = require('./profileScrape.js');
 
@@ -13,28 +14,26 @@ var GetProfileLinks = require('./GetProfileLinks_FromDump.js');
 var baseURL = 'https://www.lendwithcare.org/';
 
 //test projattr class is exposed
-console.log( projectAttr);
+//console.log( projectAttr);
 
 // get array of funded project profile links
 var linksArr = GetProfileLinks.getProfileLinks;
 
-//array to hold project data
-var profileDataRaw = [];
-
-
+var wstream = fs.createWriteStream('myOutput.txt');
 //scrape profiles
-for (var i = 0; i < 20; i++ ) {
-    //concatenate base url with profile link url
+for (var i = 0; i < 80; i++ ) {
+    //concatenate base url with profile link url to give request url
     var urlCur = baseURL + linksArr[i];
 
-    profileDataRaw.push(scrape.profileScrape(urlCur));
+// set i to determine last call in order to time file write to end of scraping operation
+    if(i !== 79){ var t = scrape.profileScrape(urlCur);}
+        else{var t = scrape.profileScrape(urlCur, true);}
+
+    //setTimeout(null,5000);
 }
 
 
-// array to hold individual profile data
-//var profilesArr = [];
-
-//loop through
-
 // write key project attributes to JSON file for completed projects
 //fs.write('CompletedProjects.json', JSON.stringify(), 'w');
+
+
