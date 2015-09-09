d05aa4bda52f4c31179eb0cc8b815fe236340fc1
TomChambrier <tomw08@gmail.com>
2015-09-09 13:21:09 +0100

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,39 +1,43 @@
+//Gets links and writes dump of profile data to file
+//
 // import modules.
-//var Q = require('q');
-
 var fs = require('fs');
-var Promise = require("bluebird");
 require('./ProjectClass.js');
+//var p = require('./parseProjDatafromJSON.js');
 var scrape = require('./profileScrape.js');
-
+var request = require("request");
 
 // import function to scrape profile
 var GetProfileLinks = require('./GetProfileLinks_FromDump.js');
-
 // all profile urls consist of this url
 var baseURL = 'https://www.lendwithcare.org/';
-
-//test projattr class is exposed
-//console.log( projectAttr);
-
 // get array of funded project profile links
 var linksArr = GetProfileLinks.getProfileLinks;
+var t = [];
+// initialise writable stream
 
-var wstream = fs.createWriteStream('myOutput.txt');
+function writeToFile(data) {
+    fs.writeFile('rawProfileContent2.txt', data, function () {
+        console.log('file written!!!!!!!!');
+    })
+}
+//count responses received
+var responseCount = 0;
 //scrape profiles
-for (var i = 0; i < 80; i++ ) {
+for (var i = 0; i < 40; i++) {
+
     //concatenate base url with profile link url to give request url
     var urlCur = baseURL + linksArr[i];
 
-// set i to determine last call in order to time file write to end of scraping operation
-    if(i !== 79){ var t = scrape.profileScrape(urlCur);}
-        else{var t = scrape.profileScrape(urlCur, true);}
+    request(urlCur, function(error, response, body){
+            t.push(body);
+            // increment response counter to keep track of responses received
+            if(response){responseCount++}
+           // this should be set to (last loop value -1) to ensure file is written after
+          // all responses have been received
+            if(responseCount === 39){writeToFile(t)}
+        });
 
-    //setTimeout(null,5000);
 }
-
-
-// write key project attributes to JSON file for completed projects
-//fs.write('CompletedProjects.json', JSON.stringify(), 'w');
 
 
