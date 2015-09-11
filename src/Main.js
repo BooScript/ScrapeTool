6a561d40dbd39b4be4e72e5473f71a1087e1ab63
TomChambrier <tomw08@gmail.com>
2015-09-11 19:27:14 +0100

    changed for loop to async.foreach

--- src/Main.js
+++ src/Main.js
@@ -13,9 +13,10 @@ function d(callback) {
         fs.writeFile('rawProfileContent2.txt', data, function () {
             console.log('file written!!!!!!!!');
             //split.split();
-        callback();
+            callback();
         })
     }
+
 // import function to get links
     var GetProfileLinks = require('./GetProfileLinks_FromDump.js');
 // calls function from getlinksfromdump module that parses JSON object and returns array of profile links
@@ -25,30 +26,49 @@ function d(callback) {
 // t termporarily stoes data for each scrape to dump to file
     var t = [];
 
-//count responses received
-    var responseCount = 0;
-//scrape profiles
-    for (var i = 0; i < 40; i++) {
-        //concatenate base url with profile link url to give request url
-        var urlCur = baseURL + linksArr[i];
 
+    /*
+
+     //count responses received
+     var responseCount = 0;
+     //scrape profiles
+     for (var i = 0; i < 40; i++) {
+     //concatenate base url with profile link url to give request url
+     var urlCur = baseURL + linksArr[i];
+
+     request(urlCur, function (error, response, body) {
+     t.push(body);
+     // increment response counter to keep track of responses received
+     if (response) {
+     responseCount++
+     }
+     // this should be set to (last loop value -1) to ensure file is written after
+     // all responses have been received
+     if (responseCount === 39) {
+     writeToFile(t, callback);
+
+     }
+     });
+     }
+     */
+
+    async.forEach((linksArr), function (url, callback) { //The second argument (callback) is the continues the control flow
+        var urlCur = baseURL + url;
+        console.log(urlCur);
         request(urlCur, function (error, response, body) {
             t.push(body);
-            // increment response counter to keep track of responses received
-            if (response) {
-                responseCount++
-            }
-            // this should be set to (last loop value -1) to ensure file is written after
-            // all responses have been received
-            if (responseCount === 39) {
-                writeToFile(t, callback);
-
-            }
+            console.log(url);
+        }, function (err) {
+            //When all requests done
+            if(err) console.log('error with request for' + url);
+           // console.log('finished!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+           // callback()
         });
-    }
 
+    });
+    callback();
 }
-async.series([
+    async.series([
         function tom(callback){
             // get links from casper links collection and scrape each profile then dump all contents
             console.log('step one!');
@@ -63,12 +83,12 @@ async.series([
             split.split ( function() {
                 console.log('calling final step');
                 callback(null, 'three');
-             });
+            });
         },
         function writeAttributesJSON(callback){
             // get links from casper links collection and scrape each profile then dump all contents
             console.log('step three!');
-           p.parsey(function(){
+            p.parsey(function(){
                 console.log('ok final parse done');
                 callback(null, 'four');});
         }
@@ -77,4 +97,3 @@ async.series([
     function(err, results){
         // results is now equal to ['one', 'two']
     });
-
