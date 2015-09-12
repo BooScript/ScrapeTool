52bbdef6ef2da326f6430403fd9401d8280a00d5
TomChambrier <tomw08@gmail.com>
2015-09-12 16:03:47 +0100

    Update GetProfileLinks_FromDump.js

--- src/GetProfileLinks_FromDump.js
+++ src/GetProfileLinks_FromDump.js
@@ -5,21 +5,40 @@ var fs = require('fs');
 
 module.exports.getProfileLinks = getProfileLinks();
 
-function getProfileLinks() {
-    //function for filtering unique links
-    function onlyUnique(value, index, self) {
-        return self.indexOf(value) === index;
-    }
+//read json file
+function readLinksfromCasper(){
+ return JSON.parse(fs.readFileSync('linksData2.json', 'utf8'));
+}
+//function for filtering unique links
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
 
-    //read json file
-    var linksdump = JSON.parse(fs.readFileSync('linksData2.json', 'utf8'));
-    //regex expression to filter only profile link
-    var profilelinks = linksdump.filter(function (e) {
-        if (e.search(/entrepreneurs.index.\d+/) !== -1) return true;
-    });
+//regex expression to filter only profile link from bunch of links
+function filterProfileLinks(links) {
+    //check arg1 is array
+    if(Array.isArray(links) === false){
+        throw TypeError('arg1 needs to be an array');
+    }
+    // iterate through elements of array
+   var filteredArr =  links.filter(function (element) {
+             //if position of link is -1 (null) then we filter it out f our array
+            if (element.search(/entrepreneurs.index.\d+/) !== -1) {
+                return true;
+                }
+             });
+
+    return filteredArr;
+}
+
+function getProfileLinks() {
+
+    var linksdump = readLinksfromCasper();
+
+    var profilelinks = filterProfileLinks(linksdump);
     //filter for unique links (delete duplicates)
     var uniqueProfileLinks = profilelinks.filter( onlyUnique ); // returns ['a', 1, 2, '1']
-    console.log(uniqueProfileLinks);
+
     return uniqueProfileLinks;
 }
 
